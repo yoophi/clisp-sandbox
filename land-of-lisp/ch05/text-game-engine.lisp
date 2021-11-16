ea2776be44f67f6d2930c27a0f0217b1ae18e021
Pyunghyuk Yoo <yoophi@gmail.com>
2021-11-16 23:45:30 +0900

    :sparkles: NEW: describe-location

--- land-of-lisp/ch05/text-game-engine.lisp
+++ land-of-lisp/ch05/text-game-engine.lisp
@@ -4,3 +4,6 @@
 				 there is a well in front of you.))
 			(attic (you are in the attic.
 				there is a giant welding torch in the corner.))))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
