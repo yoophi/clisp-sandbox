004ce300f2c7a9575446732bd6dfa2f4543688a1
Pyunghyuk Yoo <yoophi@gmail.com>
2021-11-17 22:31:27 +0900

    :sparkles: NEW: describe obejcts at locations

--- land-of-lisp/ch05/text-game-engine.lisp
+++ land-of-lisp/ch05/text-game-engine.lisp
@@ -27,3 +27,9 @@
   (labels ((at-loc-p (obj)
 	     (eq (cadr (assoc obj obj-locs)) loc)))
     (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels
+      ((describe-obj (obj)
+			`(you see a ,obj on the floor.)))
+	 (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
