24b33d07dd6dcbf5fca47d69023aff53c0e2e9c5
Pyunghyuk Yoo <yoophi@gmail.com>
2021-11-21 20:28:15 +0900

    :bug: FIX: fix error of nodes->dot fn

--- land-of-lisp/ch07/graph-util.lisp
+++ land-of-lisp/ch07/graph-util.lisp
@@ -1,3 +1,15 @@
+(defparameter *wizard-nodes* '((living-room (you are in the living-room.
+				      a wizard is snoring loudly on the couch.))
+			(garden (you are in a beautiful garden.
+				 there is a well in front of you.))
+			(attic (you are in the attic.
+				there is a giant welding torch in the corner.))))
+
+(defparameter *wizard-edges* '((living-room (garden west door)
+			 (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
+
 (defun dot-name (exp)
   (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
 
@@ -15,7 +27,10 @@
   (mapc (lambda (node)
 	  (fresh-line)
 	  (princ (dot-name (car node)))
-	  (princ "label=\"")
+	  (princ "[label=\"")
 	  (princ (dot-label node))
 	  (princ "\"];"))
 	nodes))
+
+;; (nodes->dot *wizard-nodes*)
+
